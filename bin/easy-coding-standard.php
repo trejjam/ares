68d0ae77e36e7b96b9f02e09660631507678a4cf
Jan Trejbal <jan@trejbal.land>
2022-06-25 15:41:54 +0200

    Update easy-coding-standard.php

--- bin/easy-coding-standard.php
+++ bin/easy-coding-standard.php
@@ -11,45 +11,41 @@ use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
 use PhpCsFixer\Fixer\Operator\NewWithBracesFixer;
 use PhpCsFixer\Fixer\PhpTag\BlankLineAfterOpeningTagFixer;
 use PhpCsFixer\Fixer\Whitespace\NoSpacesInsideParenthesisFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 $rootDir = __DIR__ . '/../';
 
-return static function (ContainerConfigurator $containerConfigurator) use ($rootDir) : void {
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/array.php');
+return static function (ECSConfig $ecsConfig) use ($rootDir) : void {
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/array.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/control-structures.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/control-structures.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/docblock.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/docblock.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/namespaces.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/namespaces.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/strict.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/common/strict.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/clean-code.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/clean-code.php');
 
-    $containerConfigurator->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/psr12.php');
+    $ecsConfig->import($rootDir . 'vendor/symplify/easy-coding-standard/config/set/psr12.php');
 
-    $parameters = $containerConfigurator->parameters();
+    $ecsConfig->indentation('spaces');
 
-    $parameters->set(Option::INDENTATION, 'spaces');
-
-    $parameters->set(Option::SKIP, [
-        BlankLineAfterOpeningTagFixer::class     => null,
-        ArrayOpenerAndCloserNewlineFixer::class  => null,
-        NewWithBracesFixer::class                => null,
-        ControlStructureSpacingSniff::class      => null,
-        ForEachLoopDeclarationSniff::class       => null,
-        BracesFixer::class                       => null,
-        OrderedClassElementsFixer::class         => null,
-        BinaryOperatorSpacesFixer::class         => null,
-        NoSpacesInsideParenthesisFixer::class    => null,
+    $ecsConfig->skip([
+        BlankLineAfterOpeningTagFixer::class => null,
+        ArrayOpenerAndCloserNewlineFixer::class => null,
+        NewWithBracesFixer::class => null,
+        ControlStructureSpacingSniff::class => null,
+        ForEachLoopDeclarationSniff::class => null,
+        BracesFixer::class => null,
+        OrderedClassElementsFixer::class => null,
+        BinaryOperatorSpacesFixer::class => null,
+        NoSpacesInsideParenthesisFixer::class => null,
     ]);
 
-    $services = $containerConfigurator->services();
-
-    $services->set(ReturnTypeDeclarationFixer::class)
-             ->call('configure', [['space_before' => 'one']]);
+    $ecsConfig->ruleWithConfiguration(ReturnTypeDeclarationFixer::class, [
+        'space_before' => 'one',
+    ]);
 };
